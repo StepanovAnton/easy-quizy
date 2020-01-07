8aed33b51dd7d690c8ef1f72e0def8748368619b
Developer 10 <Developer10@defa.ru>
2020-01-07 16:00:05 +0500

    Добавил сид

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Game;
+use App\Models\Team;
+use App\Models\TeamUser;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         factory(User::class, 5)->create();
         factory(Game::class, 10)->create();
+        factory(Team::class, 1)->create();
+        factory(TeamUser::class, 1)->create();
     }
 }
